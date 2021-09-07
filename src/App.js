144b54663923bf95e9cff62d1b7a3bc885dec386
Sumeer-007 <90249863+Sumeer-007@users.noreply.github.com>
2021-09-07 15:04:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Post from "./Post";
+import { initializeApp } from "firebase/app";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import "./App.css";
+import { Button, Input } from "@material-ui/core";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import ImageUpload from "./imageUpload";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const app = initializeApp({
+    apiKey: "AIzaSyAje7uu769QhvXaaEKvaS-W0GR9nXohsrM",
+    authDomain: "instagram-clone-react-pwa.firebaseapp.com",
+    projectId: "instagram-clone-react-pwa",
+    storageBucket: "instagram-clone-react-pwa.appspot.com",
+    messagingSenderId: "53880491842",
+    appId: "1:53880491842:web:c485290897ed4dc40e02db",
+    measurementId: "G-W0ZD5RNRC4",
+  });
+  const db = getFirestore(app);
+  const auth = getAuth();
+  const classes = useStyles();
+
+  const [modalStyle] = useState(getModalStyle);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        setUser(user);
+        if (user.displayName) {
+        } else {
+          return updateProfile(user, {
+            displayName: username,
+          });
+        }
+      } else {
+        setUser(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [user, username]);
+
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    onSnapshot(q, (querySnapshot) => {
+      setPosts(
+        querySnapshot.docs.map((item) => ({ key: item.key, ...item.data() }))
+      );
+    });
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((user) => updateProfile(user.user, { displayName: username }))
+      .catch((error) => {
+        alert(error.message);
+      });
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).catch((error) =>
+      alert(error.message)
+    );
+    setOpenSignIn(false);
+  };
+
+  const signOut = () => {
+    auth.signOut();
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)} className="modal">
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagaram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              SignUp
+            </Button>
+          </form>
+        </div>
+      </Modal>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagaram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Modal>
+      <div className="app__header">
+        <h4>തീപന്തം 🔥</h4>
+        {user ? (
+          <Button onClick={signOut}>Logout</Button>
+        ) : (
+          <div>
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
+      </div>
+
+      {user?.displayName && (
+        <div className="app__posts">
+          <div className="app__postsLeft">
+            {posts?.map((post) => (
+              <Post {...post} />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <center>Sorry, Please Login</center>
+      )}
     </div>
   );
 }
---
+++ src/pages/ImageUpload/imageUpload.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
+import "./imageUpload.css";
+
+export const ImageUpload = ({ username }) => {
+  const app = initializeApp({
+    apiKey: "AIzaSyAje7uu769QhvXaaEKvaS-W0GR9nXohsrM",
+    authDomain: "instagram-clone-react-pwa.firebaseapp.com",
+    projectId: "instagram-clone-react-pwa",
+    storageBucket: "instagram-clone-react-pwa.appspot.com",
+    messagingSenderId: "53880491842",
+    appId: "1:53880491842:web:c485290897ed4dc40e02db",
+    measurementId: "G-W0ZD5RNRC4",
+  });
+  const db = getFirestore(app);
+  const storage = getStorage();
+
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [caption, setCaption] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    const storageRef = ref(storage, `images/${image.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 10
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        alert(error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          addDoc(collection(db, "posts"), {
+            timestamp: serverTimestamp(),
+            caption: caption,
+            imageUrl: downloadURL,
+            username: username,
+          });
+          setProgress(0);
+          setCaption("");
+          setImage(null);
+        });
+      }
+    );
+  };
+
+  return (
+    <div className="imageupload">
+      <progress className="imageupload__progress" value={progress} max="100" />
+      <input
+        type="text"
+        placeholder="Enter a caption"
+        value={caption}
+        onChange={(e) => setCaption(e.target.value)}
+      />
+      <input type="file" onChange={handleChange} />
+      <Button onClick={handleUpload}>Upload</Button>
+    </div>
+  );
+};
+
+export default ImageUpload;
